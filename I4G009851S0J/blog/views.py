83db26d8785ee59507f783649c2e8ab584006b5c
Ngozi1 <103275987+Ngozi1@users.noreply.github.com>
2022-06-29 05:21:17 +0100

    this is my final commit

--- I4G009851S0J/blog/views.py
+++ I4G009851S0J/blog/views.py
@@ -1,28 +1,41 @@
 from django.shortcuts import render
+from pyexpat import model
 from django.urls import reverse_lazy
-from django.views import generic
-from models import Post
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic import DetailView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from blog.models import Post
+from .models import *
+
 
 # Create your views here.
 
 
-class PostListView(generic.Listview):
-  model=  Post
+class PostListView(ListView):
+    model = Post
+template_name = "blog/post_list.html"
 
-class PostCreateView(generic.Createview):
-  model= Post
-  fields="_all_"
-success_url=reverse_lazy("blog:all")
 
-class PostDetailView(generic.Detailview):
-  model= Post
+class PostCreateView(CreateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog:all')
+template_name = "blog/post_form.html"
 
-class PostUpdateView(generic.Updateview):
-   model= Post
-   fields="_all_"
-   success_url=reverse_lazy("blog:all")
+class PostDetailView(DetailView):
+    model = Post
+template_name = "blog/post_detail.html"
 
-class PostDeleteView(generic.Deleteview):
-  model= Post
-  fields="_all_"
-  success_url=reverse_lazy("blog:all")
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog:all')
+template_name = "blog/post_form.html"
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = '__all__'
+    success_url = reverse_lazy('blog:all')
+template_name = "blog/post_confirm_delete.html"
